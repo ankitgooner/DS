540cf14aacb44d90dd3a29daa666331b4f6bcb15
ankit chauhan <ankit.persie@gmail.com>
2017-03-05 15:16:37 +0530

    LCS

---
+++ src/DP/LCS.java
@@ -0,0 +1,44 @@
+package DP;
+
+public class LCS {
+
+	public int max(int i,int j){
+		
+		return i>j?i:j;
+		
+	}
+	
+	public  int calcLCS(char a[],char b[]){
+		
+		int n=a.length;int m=b.length;
+		
+		int lcs[][]=new int[n+1][m+1];
+		
+		for(int i=0;i<=n;i++){
+			for(int j=0;j<=m;j++){
+				
+				if(i==0 || j==0){
+					
+					lcs[i][j]=0;
+				}
+				else if(a[i-1]==b[j]){
+					lcs[i][j]=lcs[i-1][j-1]+1;
+				}
+				else{
+					lcs[i][j]=max(lcs[i-1][j],lcs[i][j-1]);
+					
+				}
+			}
+		}
+		return lcs[n][m];
+	}
+	public static void main(String p[]){
+		char a[]={'A','B','C','D','E','F','G','H'};
+		char b[]={'X','B','E','G','H'};	
+		
+		System.out.println(new LCS().calcLCS(a, b));
+		}
+		
+		
+	}
+
--- src/DP/LIS.java
+++ src/DP/LIS.java
@@ -1,38 +1,36 @@
 package DP;
 
 public class LIS {
-	
+
 	public static void main(String a[]){
 		int maxlength=1;
-	int n[]={50,8,9,5,7,6,7,8,10,11};
-	int len=n.length;
-	int table[]=new int[len];
-	for(int i =0;i<len;i++){
+		int n[]={50,8,9,5,7,6,7,8,10,11};
+		int len=n.length;
+		int table[]=new int[len];
 		
-		table[i]=1;
-	}
-	for(int i=0;i<len-1;i++){
-		for(int j=i+1;j<len;j++){
-			if(n[j]>n[i] && n[j]>n[j-1]){
-				
-				table[i]+=1;
-				
+		for(int i =0;i<len;i++){
+			table[i]=1;
+		}
+		
+		for(int i=0;i<len-1;i++){
+			for(int j=i+1;j<len;j++){
+				if(n[j]>n[i] && n[j]>n[j-1]){
+ 				 table[i]+=1;
+				}
 			}
-			
+			if(max(table[i],maxlength)){
+				maxlength=table[i];
+			}
 		}
-		System.out.println(i+": "+table[i]);
-		if(max(table[i],maxlength)){
-						maxlength=table[i];
-		}
+
+		System.out.println("Max Length"+maxlength);
 	}
 	
-System.out.println("Max Length"+maxlength);
-	}
 	public static  boolean max(int i,int j){
-		
-	boolean p=(i>j)?true:false;
-	
-	return p;
+
+		boolean p=(i>j)?true:false;
+
+		return p;
 	}
 
 }
